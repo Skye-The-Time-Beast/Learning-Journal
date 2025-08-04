b3ff37de8a59bf9a3cf6894eff32d389a90d98c0
SkyeTheTimeBeast <212593120+Skye-The-Time-Beast@users.noreply.github.com>
2025-08-04 12:41:48 +0100

    update

--- projects/rust/guessing_game/src/main.rs
+++ projects/rust/guessing_game/src/main.rs
@@ -1,3 +1,23 @@
+// IO (input output library) being added from the standard libary (std)
+use std::io;
+// main function is the entry point into the program
+//fn syntax is declaring a new function, the () indicating the parameters (in this case, none)
+// { starts the body of a function
 fn main() {
-    println!("Hello, world!");
+    // println! is a macro to print to screen
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+    // "let" statement creates a variable
+    // "mut" makes said variable 'mutable' meaning changeable and not fixed
+    // "guess" is the name of the variable
+    //you declare what type of variable, in this case a String
+    //
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guess: {guess}");
 }
